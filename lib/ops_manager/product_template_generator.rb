894758480958d442ede4c861f2fd74fe949bf7ab
Alan Moran <nebsy@allstate.com>
2016-06-14 08:48:41 -0500

    Wip - product template generator
    Signed-off-by: Lukasz Rabczak <lukasz.rabczak@gmail.com>

--- lib/ops_manager/product_template_generator.rb
+++ lib/ops_manager/product_template_generator.rb
@@ -1,5 +1,8 @@
 class OpsManager
   class ProductTemplateGenerator
+    OPS_MANAGER_PASSWORD_LENGTH = 20
+    OPS_MANAGER_SALT_LENGTH = 16
+
     attr_reader :product_name
 
     def initialize(product_name)
@@ -7,17 +10,75 @@ class OpsManager
     end
 
     def generate
-      {
-        'products' => [
-          "(( merge on guid ))" ,
-          installation_settings.fetch('products').select do |p|
-            p.fetch('identifier') == product_name
-          end.first
-        ]
-      }.to_yaml
+      delete_partitions
+      delete_vm_credentials
+      delete_jobs_guid
+      delete_prepared
+      delete_ops_manager_generated_passwords
+      delete_ops_manager_generated_salts
+      add_merging_strategy_for_jobs
+
+      { 'products' => [ "(( merge on guid ))" , selected_product ] }.to_yaml
+        .gsub('"(( merge on guid ))"', '(( merge on guid ))')
+        .gsub('"(( merge on identifier ))"', '(( merge on identifier ))')
     end
 
     private
+    def delete_ops_manager_generated_passwords
+      delete_value_from_properties_if do |value|
+        value.fetch('password', '').length == OPS_MANAGER_PASSWORD_LENGTH
+      end
+    end
+
+    def delete_ops_manager_generated_salts
+      delete_value_from_properties_if do |value|
+        value.fetch('salt', '').length == OPS_MANAGER_SALT_LENGTH
+      end
+    end
+
+    def delete_value_from_properties_if
+      selected_product['jobs'].each do |j|
+        j.fetch('properties', []).each  do |p|
+          value = p.fetch('value',{})
+          p.delete('value') if value.is_a?(Hash) && yield(value)
+        end
+      end
+    end
+
+    def delete_prepared
+      selected_product.delete("prepared")
+    end
+
+    def add_merging_strategy_for_jobs
+      selected_product['jobs'].unshift("(( merge on identifier ))")
+    end
+
+    def delete_partitions
+      delete_from_jobs('partitions')
+    end
+
+    def delete_vm_credentials
+      delete_from_jobs('vm_credentials')
+    end
+
+    def delete_jobs_guid
+      delete_from_jobs('guid')
+    end
+
+    def selected_product
+      @selected_product ||= products.select {|p| p.fetch('identifier') == product_name }.first
+    end
+
+    def products
+      installation_settings.fetch('products')
+    end
+
+    def delete_from_jobs(key)
+      selected_product.tap do |sp|
+        sp['jobs'].each{ |j| j.delete(key) }
+      end
+    end
+
     def installation_settings
       parsed_installation_settings = JSON.parse(installation_settings_response.body)
       OpsManager::InstallationSettings.new(parsed_installation_settings)
--- spec/ops_manager/product_template_generator_spec.rb
+++ spec/ops_manager/product_template_generator_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe OpsManager::ProductTemplateGenerator do
   let(:product_template_generator){ described_class.new(product_name) }
   let(:product_name){'dummy-product'}
-  let(:product_template){"---\nproducts:\n- (( merge on guid ))\n- identifier: #{product_name}\n"}
-  let(:installation_settings){ {'some' => 'key', 'products' => [{ 'identifier' =>product_name }] } }
+  let(:product_template){"---\nproducts:\n- (( merge on guid ))\n- identifier: #{product_name}\n  jobs:\n  - (( merge on identifier ))\n"}
+  let(:installation_settings){ { 'products' => [{ 'prepared' => true,'identifier' =>product_name, 'jobs' => [] }] } }
   let(:installation_response){ double(code: 200, body: installation_settings.to_json) }
 
   describe '#generate' do
@@ -18,6 +18,128 @@ describe OpsManager::ProductTemplateGenerator do
 
     it "should return template" do
       expect(product_template_generator.generate).to eq(product_template)
+    end
+
+
+    it 'should remove prepared entry' do
+      expect(product_template_generator.generate.to_s).not_to match('prepared')
+    end
+
+    describe 'when products have partitions' do
+      let(:installation_settings)  do
+        { 'prepared' =>  true , 'products' => [
+          { 'identifier' => product_name ,
+            'jobs' => [
+              { 'identifier' => 'example-job' ,
+                'partitions' => 'some partition info' }
+          ]
+        }
+        ]
+        }
+      end
+
+      it 'should remove partitions entry' do
+        expect(product_template_generator.generate.to_s).not_to match('some partition info')
+      end
+    end
+
+    describe 'when products have vm_credentials' do
+      let(:installation_settings)  do
+        { 'prepared' =>  true , 'products' => [
+          { 'identifier' => product_name ,
+            'jobs' => [
+              { 'identifier' => 'example-job' ,
+                'vm_credentials' => 'some vm credentials' }
+          ]
+        }
+        ]
+        }
+      end
+
+      it 'should remove vm_credentials entry' do
+        expect(product_template_generator.generate.to_s).not_to match('some vm credentials')
+      end
+    end
+
+    describe 'when products have guid' do
+      let(:installation_settings)  do
+        { 'prepared' =>  true , 'products' => [
+          { 'identifier' => product_name ,
+            'jobs' => [
+              { 'identifier' => 'example-job' ,
+                'guid' => 'job-guid-example' }
+          ]
+        }
+        ]
+        }
+      end
+
+      it 'should remove guid entry' do
+        expect(product_template_generator.generate.to_s).not_to match('job-guid-example')
+      end
+    end
+
+    describe 'when product job properties has passwords' do
+      let(:random_password){ 'dd2bc21ac11d607d73c8' }
+      let(:custom_password){ 'custom-password' }
+      let(:installation_settings)  do
+        { 'products' => [
+          { 'identifier' => product_name ,
+            'jobs' => [
+              { 'identifier' => 'example-job' ,
+                'properties' => [
+                  {
+                    'value' => {
+                      'identity' => 'conf-1',
+                      'password' => random_password
+                    }
+                  },
+                  {
+                    'value' => {
+                      'identity' => 'conf-2',
+                      'password' => custom_password
+                    }
+                  }
+              ]
+          }
+          ]
+        }
+        ]}
+      end
+
+      it 'should remove ops_manager generated passwords' do
+        expect(product_template_generator.generate.to_s).not_to match(random_password)
+      end
+
+      it 'should not remove custom passwords' do
+        expect(product_template_generator.generate.to_s).to match('custom-password')
+      end
+    end
+
+    describe 'when product job properties has salt' do
+      let(:random_salt){ '1234123412341234' }
+      let(:installation_settings)  do
+        { 'products' => [
+          { 'identifier' => product_name ,
+            'jobs' => [
+              { 'identifier' => 'example-job' ,
+                'properties' => [
+                  {
+                    'value' => {
+                      'identity' => 'conf-1',
+                      'salt' => random_salt
+                    }
+                  }
+              ]
+          }
+          ]
+        }
+        ]}
+      end
+
+      it 'should remove automatically generated salt' do
+        expect(product_template_generator.generate.to_s).not_to match(random_salt)
+      end
     end
   end
 end
