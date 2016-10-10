32eacabf38b4c4c1041d59a38c4c6b15d0aed930
Alan Moran <bonzofenix@gmail.com>
2016-10-10 02:39:01 -0300

    Removes VCR

--- ops_manager.gemspec
+++ ops_manager.gemspec
@@ -20,7 +20,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.10"
   spec.add_development_dependency "rake"
   spec.add_development_dependency "rspec"
-  spec.add_development_dependency "vcr"
   spec.add_development_dependency "webmock"
   spec.add_development_dependency "byebug"
   spec.add_dependency "rbvmomi"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,31 +1,10 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'ops_manager'
-require 'vcr'
 require 'webmock/rspec'
 
 RSpec.configure do |config|
-  # config.before :all do
-    # @orig_stdout = $stdout
-    # @orig_stderr = $stderr
-
-    # $stdout = File.open(File::NULL, "w")
-    # $stderr = File.open(File::NULL, "w")
-  # end
-
-  # config.after :all do
-   # $stdout = @orig_stdout
-   # $stderr = @orig_stderr
-  # end
-
   config.before :suite do
     Dir.chdir('spec/dummy')
     ENV['HOME'] = ENV['PWD']
   end
 end
-
-VCR.configure do |config|
-  # config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
-  config.allow_http_connections_when_no_cassette = true
-  config.hook_into :webmock
-end
-
